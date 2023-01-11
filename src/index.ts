ce60fb421021b629a842f8d90605ed757fcf9e09
Javier Vazquez Fernandez <javier@MacBook-Pro-de-Javier-2.local>
2023-01-11 13:11:50 +0100

    Add health check

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,55 @@
+// npm install @apollo/server express graphql cors body-parser
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { typeDefs, resolvers } from './schema';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
-
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+interface MyContext {
+    token?: String;
+}
 
 const run = async () => {
-    const {url} = await startStandaloneServer(server, {
-        listen: {port: 5000},
+    // Required logic for integrating with Express
+    const app = express();
+    // Our httpServer handles incoming requests to our Express app.
+    // Below, we tell Apollo Server to "drain" this httpServer,
+    // enabling our servers to shut down gracefully.
+    const httpServer = http.createServer(app);
+
+    // Same ApolloServer initialization as before, plus the drain plugin
+    // for our httpServer.
+    const server = new ApolloServer<MyContext>({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    // Ensure we wait for our server to start
+    await server.start();
+
+    // Set up our Express middleware to handle CORS, body parsing,
+    // and our expressMiddleware function.
+    // Requests to `http://localhost:4000/health` now return "Okay!"
+    app.get('/health', (req, res) => {
+        res.status(200).send('Okay!');
     });
 
-    console.log(`🚀  Server ready at: ${url}`);
+    app.use(
+        '/',
+        cors<cors.CorsRequest>(),
+        bodyParser.json(),
+        // expressMiddleware accepts the same arguments:
+        // an Apollo Server instance and optional configuration options
+        expressMiddleware(server, {
+            context: async ({ req }) => ({ token: req.headers.token }),
+        }),
+    );
+
+    // Modified server startup
+    await new Promise<void>((resolve) => httpServer.listen({ port: 5000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:5000/`);
 }
 run()
